d86bcca2f76c36dc08cbfc5edb300e4f13797b29
mrunal08 <mrunalg96@gmail.com>
2021-08-11 14:51:34 +0530

    Emplyee Present/Absent UC1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,22 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    //main method
+    public static void main(String[] args){
         System.out.println("Welcome to Employee Wage Computation Program");
+        int FT=1;
+        int EMP=10;
+
+        double totalHours=(EMP*10)%2;
+     
+        if(FT==totalHours){
+            System.out.println("Employee Absent");
+        }
+        else {
+            System.out.println("Employee Present");
+        }
+
+
     }
 }
+
